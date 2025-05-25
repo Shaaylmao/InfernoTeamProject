880941b17c43f609b0a1fa262ad729c81d98fbed
Aayan Sharma <aayan37962@gmail.com>
2025-05-25 14:57:59 +0200

    more

--- Inferno/Assets/Scripts/PlayerController.cs
+++ Inferno/Assets/Scripts/PlayerController.cs
@@ -6,9 +6,12 @@ using UnityEngine.AI;
 
 public class PlayerController : MonoBehaviour
 {
-   
     const string IDLE = "Idle";
     const string JOG = "Jog";
+    const string ATTACK = "Attack";
+    const string PICKUP = "Pickup";
+
+    string currentAnimation;
 
     CustomAction input;
 
@@ -20,6 +23,16 @@ public class PlayerController : MonoBehaviour
     [SerializeField] LayerMask clickableLayers;
 
     float lookRotationSpeed = 8f;
+
+    [Header("Attack")]
+    [SerializeField] float attackSpeed = 1.5f;
+    [SerializeField] float attackDelay = 0.3f;
+    [SerializeField] float attackDistance = 1.5f;
+    [SerializeField] int attackDamage = 1;
+    [SerializeField] ParticleSystem hitEffect;
+
+    bool playerBusy = false;
+    Interactable target;
 
     void Awake()
     {
@@ -40,9 +53,20 @@ public class PlayerController : MonoBehaviour
         RaycastHit hit;
         if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, 100, clickableLayers))
         {
-            agent.destination = hit.point;
-            if (clickEffect != null)
-            { Instantiate(clickEffect, hit.point + new Vector3(0, 0.1f, 0), clickEffect.transform.rotation); }
+            if (hit.transform.CompareTag("Interactable"))
+            {
+                target = hit.transform.GetComponent<Interactable>();
+                if (clickEffect != null)
+                { Instantiate(clickEffect, hit.transform.position + new Vector3(0, 0.1f, 0), clickEffect.transform.rotation); }
+            }
+            else
+            {
+                target = null;
+
+                agent.destination = hit.point;
+                if (clickEffect != null)
+                { Instantiate(clickEffect, hit.point + new Vector3(0, 0.1f, 0), clickEffect.transform.rotation); }
+            }
         }
     }
 
@@ -54,19 +78,84 @@ public class PlayerController : MonoBehaviour
 
     void Update()
     {
+        FollowTarget();
         FaceTarget();
         SetAnimations();
     }
 
+    void FollowTarget()
+    {
+        if (target == null) return;
+
+        if (Vector3.Distance(target.transform.position, transform.position) <= attackDistance)
+        { ReachDistance(); }
+        else
+        { agent.SetDestination(target.transform.position); }
+    }
+
     void FaceTarget()
     {
-        Vector3 direction = (agent.destination - transform.position).normalized;
+        if (agent.destination == transform.position) return;
+
+        Vector3 facing = Vector3.zero;
+        if (target != null)
+        { facing = target.transform.position; }
+        else
+        { facing = agent.destination; }
+
+        Vector3 direction = (facing - transform.position).normalized;
         Quaternion lookRotation = Quaternion.LookRotation(new Vector3(direction.x, 0, direction.z));
         transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * lookRotationSpeed);
     }
 
+    void ReachDistance()
+    {
+        agent.SetDestination(transform.position);
+
+        if (playerBusy) return;
+
+        playerBusy = true;
+
+        switch (target.interactionType)
+        {
+            case InteractableType.Enemy:
+
+                animator.Play(ATTACK);
+
+                Invoke(nameof(SendAttack), attackDelay);
+                Invoke(nameof(ResetBusyState), attackSpeed);
+                break;
+            case InteractableType.Item:
+
+                target.InteractWithItem();
+                target = null;
+
+                Invoke(nameof(ResetBusyState), 0.5f);
+                break;
+        }
+    }
+
+    void SendAttack()
+    {
+        if (target == null) return;
+
+        if (target.myActor.currentHealth <= 0)
+        { target = null; return; }
+
+        Instantiate(hitEffect, target.transform.position + new Vector3(0, 1, 0), Quaternion.identity);
+        target.GetComponent<Actor>().TakeDamage(attackDamage);
+    }
+
+    void ResetBusyState()
+    {
+        playerBusy = false;
+        SetAnimations();
+    }
+
     void SetAnimations()
     {
+        if (playerBusy) return;
+
         if (agent.velocity == Vector3.zero)
         { animator.Play(IDLE); }
         else
