a33b4a4724d26dca13616428b829f04ee461a7c0
Aayan Sharma <aayan37962@gmail.com>
2025-05-31 15:14:06 +0200

    EnemyTargeting

---
+++ Inferno/Assets/Scripts/Targetable.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+
+public class Targetable : MonoBehaviour
+{
+    public string DisplayName = "Enemy";
+    public float MaxHealth = 100f;
+    public float CurrentHealth = 100f;
+
+    public bool IsTargeted { get; private set; }
+
+    [SerializeField] private Renderer targetRenderer;
+    [SerializeField] private Color normalColor = Color.white;
+    [SerializeField] private Color highlightColor = Color.red;
+
+    private void Awake()
+    {
+        if (targetRenderer == null)
+            targetRenderer = GetComponentInChildren<Renderer>();
+    }
+
+    public void SetTargeted(bool value)
+    {
+        IsTargeted = value;
+        if (targetRenderer != null)
+        {
+            targetRenderer.material.color = value ? highlightColor : normalColor;
+        }
+    }
+
+    public float GetHealthPercent()
+    {
+        return Mathf.Clamp01(CurrentHealth / MaxHealth);
+    }
+}
---
+++ Inferno/Assets/Scripts/TargetingSystem.cs
@@ -0,0 +1,117 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Linq;
+
+public class TargetingSystem : MonoBehaviour
+{
+    [Header("Targeting")]
+    public float maxTargetDistance = 30f;
+    public LayerMask targetableLayer;
+
+    [Header("UI")]
+    public GameObject targetUIPanel;
+    public Slider healthSlider;
+
+    private List<Targetable> availableTargets = new();
+    private int currentTargetIndex = -1;
+    private Targetable currentTarget;
+    private Camera mainCamera;
+
+    void Start()
+    {
+        mainCamera = Camera.main;
+        UpdateTargetUI(null);
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Tab))
+        {
+            FindAvailableTargets();
+            CycleTarget();
+        }
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            ClickSelectTarget();
+        }
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            ClearTarget();
+        }
+
+        if (currentTarget != null)
+        {
+            UpdateTargetUI(currentTarget);
+            if (Vector3.Distance(transform.position, currentTarget.transform.position) > maxTargetDistance)
+            {
+                ClearTarget();
+            }
+        }
+    }
+
+    void FindAvailableTargets()
+    {
+        var allTargets = FindObjectsOfType<Targetable>();
+        availableTargets = allTargets
+            .Where(t => Vector3.Distance(transform.position, t.transform.position) <= maxTargetDistance)
+            .OrderBy(t => Vector3.Distance(transform.position, t.transform.position))
+            .ToList();
+    }
+
+    void CycleTarget()
+    {
+        if (availableTargets.Count == 0) return;
+
+        ClearTarget();
+
+        currentTargetIndex = (currentTargetIndex + 1) % availableTargets.Count;
+        currentTarget = availableTargets[currentTargetIndex];
+        currentTarget.SetTargeted(true);
+        UpdateTargetUI(currentTarget);
+    }
+
+    void ClickSelectTarget()
+    {
+        Ray ray = mainCamera.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(ray, out RaycastHit hit, maxTargetDistance, targetableLayer))
+        {
+            Targetable clicked = hit.collider.GetComponentInParent<Targetable>();
+            if (clicked != null)
+            {
+                ClearTarget();
+                currentTarget = clicked;
+                currentTarget.SetTargeted(true);
+                UpdateTargetUI(currentTarget);
+            }
+        }
+    }
+
+    public void ClearTarget()
+    {
+        if (currentTarget != null)
+        {
+            currentTarget.SetTargeted(false);
+            currentTarget = null;
+        }
+        currentTargetIndex = -1;
+        UpdateTargetUI(null);
+    }
+
+    void UpdateTargetUI(Targetable target)
+    {
+        if (target == null)
+        {
+            targetUIPanel.SetActive(false);
+            return;
+        }
+
+        healthSlider.value = target.GetHealthPercent();
+        targetUIPanel.SetActive(true);
+    }
+
+    public Targetable GetCurrentTarget() => currentTarget;
+}
